ea3e0576064382556bc10875fb594ec79b5f5480
jessendasilva1 <25090776+jessendasilva1@users.noreply.github.com>
2018-12-22 17:40:10 -0500

    final revision

---
+++ README.md
@@ -0,0 +1,13 @@
+# GIFTastic Application
+
+## Author: Jessen Dasilva
+
+## University of Toronto Bootcamp
+
+### Usage
+
+1. To run my application, open the index.html file inside of your favorite web browser
+
+2. Click on one of the pre-generated buttons at the top of the page to generate list of 10 gifs
+
+3. You can also add an animal to the list of your choosing and then have that animals gif be generated to the page
--- js/index.js
+++ js/index.js
@@ -1,5 +1,60 @@
-function start(){
-    console.log('christmas is almost here!');
+var buttonArray = ["hawk", "giraffe", "dog", "cat", "rabbit", "squirrel", "lion", "hamster"];
+var animalClicked;
+var currentAnimal;
+var apikey = "oDHbUaskKcKp6fj1tNKzNusrKKFcaDRS";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
+
+
+function start() {
+    generateButtons();
+    //console.log('christmas is almost here!');
+}
+function generateButtons() {
+    $("#buttonDiv").empty();
+    $.each(buttonArray, function (index, animalName) {
+       // console.log(animalName);
+        $("#buttonDiv").append(`
+            <button id="${animalName}" type="button" class="btn animalButton" onclick="generateGifs(this)">${animalName}</button>
+        `);
+    })
 }
 
-start();
+function generateGifs(element){
+    animalClicked = element.id;
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalClicked + "&api_key=" + apikey + "&limit=10&rating=g";
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function(response){
+        gifData = response.data;
+        $("#gifDisplay").empty();
+        for(var i = 0; i < gifData.length; i++){
+            $("#gifDisplay").append(`
+            <div class="d-flex flex-column">
+                <h5 id="gifTitle">Rating: ${gifData[i].rating}</h5>
+                <img src="${gifData[i].images.fixed_height_still.url}" data-still="${gifData[i].images.fixed_height_still.url}" data-animate="${gifData[i].images.fixed_height.url}" data-state="still" class="animalGifs" alt="animal" onclick="changeGifState(this)">
+                </div
+            `);
+        }
+    })
+}
+
+function changeGifState(element){
+    var state = element.dataset;
+    if(state.state === "still"){
+        element.src = state.animate;
+        state.state = "animate"
+    }   
+    else{
+        element.src = state.still;
+        state.state = "still";
+    }
+    //console.log("gifState: " + state);
+}
+
+$("#animalSubmitButton").on("click", function(){
+    currentAnimal = $("#animalName").val();
+    $("#animalName").empty();
+    buttonArray.push(currentAnimal);
+    generateButtons();
+})
